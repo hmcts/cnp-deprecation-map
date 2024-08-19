a59659399dd01fad40c0573c0669af32b929fda3
Rees Pozzi <rees.pozzi@justice.gov.uk>
2024-08-19 15:10:57 +0100

    Fix: Script is adding new yaml attribute
    Because the nagger file format changed when moving between repos, the `[0]` is no longer needed

--- scripts/angular-version.sh
+++ scripts/angular-version.sh
@@ -43,11 +43,11 @@ if [[ $angular_version -lt $latest_supported_version ]];then
     git config user.email github-actions@github.com
     git pull
     git checkout -b angular-update
-    yq eval -i '.npm["angular/core"][0].version = '\"$latest_supported_version\" $deprecation_config_file
+    yq eval -i '.npm["angular/core"].version = '\"$latest_supported_version\" $deprecation_config_file
     # Add 30 days
     one_month_from_now=$(expr $current_date + 2592000)
     one_month_from_now=$(timestamp_to_date "$one_month_from_now")
-    yq eval -i '.npm["angular/core"][0].date_deadline = '\"$one_month_from_now\" $deprecation_config_file
+    yq eval -i '.npm["angular/core"].date_deadline = '\"$one_month_from_now\" $deprecation_config_file
     git add "$deprecation_config_file"
     git commit -m "Auto-Updating Angular Version"
     git push --set-upstream origin angular-update
